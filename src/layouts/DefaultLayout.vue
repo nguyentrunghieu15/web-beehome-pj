b6a58e20013b3000afcd0cf70aafe4535f0bc0b9
nguyentrunghieugl15 <nguyentrunghieugl15112002@gmail.com>
2024-03-27 19:04:40 +0700

    init project

---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,9 @@
+<template>
+    <v-app>
+        <AppBar/>
+    </v-app>
+</template>
+
+<script setup lang="ts">
+import AppBar from './componets/AppBar.vue';
+</script>
---
+++ src/layouts/componets/AppBar.vue
@@ -0,0 +1,11 @@
+<template>
+<v-app-bar :elevation="2">
+  <template v-slot:prepend>
+    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+  </template>
+  <v-app-bar-title>Bee Home</v-app-bar-title>
+</v-app-bar>
+</template>
+
+<script setup lang="ts">
+</script>
